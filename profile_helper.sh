b0ae7f896a7de32c61123633334a91ab93a0d371
Cameron Flint <dev@caflint.fastmail.com>
2020-03-08 21:54:28 -0700

    WIP

--- profile_helper.sh
+++ profile_helper.sh
@@ -8,11 +8,24 @@ script_dir=$(cd "$(dirname "$file_name")" && pwd)
 
 . "$script_dir/realpath/realpath.sh"
 
-if [ -f ~/.base16_theme ]; then
-  script_name=$(basename "$(realpath ~/.base16_theme)" .sh)
-  echo "export BASE16_THEME=${script_name#*-}"
-  echo ". ~/.base16_theme"
+# Specify BASE16_DUAL_TONE to select the light theme
+theme_file_name=
+if [ -v BASE16_DUAL_TONE ]; then
+  theme_file_name="~/base16_theme_light"
+else
+  theme_file_name="~/base16_theme_dark"
 fi
+
+function load_theme_file() {
+  local path=$1
+  script_name=$(basename "$(realpath $path)" .sh)
+  echo "export BASE16_THEME=${script_name#*-}"
+  echo ". $them_file_name"
+}
+# If we've already got a generated theme file, load it.
+if [ -f "$theme_file_name" ]; then
+fi
+
 cat <<'FUNC'
 _base16()
 {
@@ -29,10 +42,35 @@ _base16()
   fi
 }
 FUNC
+cat <<'FUNC'
+_base16_dual_tone()
+{
+  local script=$1
+  local theme=$2
+  [ -f $script ] && . $script
+  export BASE16_THEME=${theme}
+  local target=
+  [ -v BASE16_LIGHT ] && target=~/.base16_theme_light;
+  [ -v BASE16_DARK ] && target=~/.base16_theme_dark;
+  [ -z target ] && target=~/.base16_theme;
+  echo -e "if '$BASE16_LIGHT' != ''\n  colorscheme base16-${theme}_light\nelseif '$BASE16_DARK' != ''\n  colorscheme base16-${theme}_dark\nendif" >| ~/.vimrc_background
+  if [ -n ${BASE16_SHELL_HOOKS:+s} ] && [ -d "${BASE16_SHELL_HOOKS}" ]; then
+    for hook in $BASE16_SHELL_HOOKS/*; do
+      [ -f "$hook" ] && [ -x "$hook" ] && "$hook"
+    done
+  fi
+}
+FUNC
+
+# Set up base16_* aliases.
 for script in "$script_dir"/scripts/base16*.sh; do
   script_name=${script##*/}
   script_name=${script_name%.sh}
   theme=${script_name#*-}
   func_name="base16_${theme}"
-  echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\""
+  if [ -v BASE16_DUAL_TONE ]; then
+    echo "alias $func_name=\"_base16_dual_tone \\\"$script\\\" $theme\""
+  else
+    echo "alias $func_name=\"_base16 \\\"$script\\\" $theme\""
+  fi
 done;
